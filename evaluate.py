cfddba8d9dc51b809977ce7c101f61692106e940
Orhan Firat <orhanf@google.com>
2020-05-21 11:16:32 -0700

    Fix evaluate.py issues
    PiperOrigin-RevId: 312704963

--- evaluate.py
+++ evaluate.py
@@ -15,11 +15,16 @@
 """Evaluation."""
 
 import argparse
-from seqeval.metrics import precision_score, recall_score, f1_score
+import json
+import os
 import sys
+
+from collections import defaultdict
+from seqeval.metrics import precision_score, recall_score, f1_score
 
 from third_party.evaluate_squad import evaluate as squad_eval
 from third_party.evaluate_mlqa import evaluate as mlqa_eval
+
 
 def read_tag(file):
   labels = []
@@ -125,9 +130,9 @@ METRIC_FUNCTION = {
 def evaluate_one_task(prediction_file, label_file, task, language=None):
   """Evalute the classification tasks by accuracy.
   Args:
-    prediction_file (string): path to the prediction tsv file. 
+    prediction_file (string): path to the prediction tsv file.
     label_file (string): path to the grouth truth tsv file.
-  Return: 
+  Return:
     result (dict): a dictionary with accuracy.
 
   Both input files contain one example per line as follows:
@@ -151,22 +156,26 @@ def evaluate(prediction_folder, label_folder):
   """
   prediction_tasks = next(os.walk(prediction_folder))[1]
   label_tasks = next(os.walk(label_folder))[1]
-  
+
   detailed_scores = {}
   for task, langs in TASK2LANGS.items():
     if task in prediction_tasks and task in label_tasks:
       suffix = "json" if task in GROUP2TASK["qa"] else "tsv"
+
       # collect scores over all languages
       score = defaultdict(dict)
       for lg in langs:
         prediction_file = os.path.join(prediction_folder, task, f"test-{lg}.{suffix}")
         label_file = os.path.join(label_folder, task, f"test-{lg}.{suffix}")
         score_lg = evaluate_one_task(prediction_file, label_file, task, language=lg)
-        for metric in score_lg.items():
+        for metric, _ in score_lg.items():
           score[metric][lg] = score_lg[metric]
+
       # average over all languages
-      for m in score: 
-        score[f'avg_{m}'] = sum(score[m].values()) / len(score[m])
+      aggregage_metrics = {}
+      for m in score.keys():
+        aggregage_metrics[f'avg_{m}'] = sum(score[m].values()) / len(score[m])
+      score.update(aggregage_metrics)
       if task in GROUP2TASK["qa"]:
         score['avg_metric'] = (score['avg_exact_match'] + score['avg_f1']) / 2
       elif 'avg_f1' in score:
@@ -175,13 +184,13 @@ def evaluate(prediction_folder, label_folder):
         score['avg_metric'] = score['avg_accuracy']
       detailed_scores[task] = score
 
-  # Display logic: 
+  # Display logic:
   # If scores of all tasks in a sub group are available, show the score in the sub table
   overall_scores = {}
   for group in GROUP2TASK:
     if all(task in detailed_scores for task in group):
       overall_scores[group] = sum(detailed_scores[task]['avg_metric'] for task in group) / len(group)
-  
+
   # If scores of all tasks are available, show the overall score in the main table
   all_tasks = list(TASK2LANGS.keys())
   if all(task in all_tasks for task in detailed_scores):
@@ -198,4 +207,5 @@ if __name__ == '__main__':
     help='the grouth truth file')
   args = parser.parse_args()
   overall_scores, detailed_scores = evaluate(args.prediction_folder, args.label_folder)
-  print(overall_scores)
+  overall_scores.update(detailed_scores)
+  print(json.dumps(overall_scores))
