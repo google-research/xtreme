954aa1701ddaca2a81f5570276a0053e01b2b67f
Jannis Vamvas <jvamvas@users.noreply.github.com>
2020-09-10 20:16:49 +0200

    Fix typo in train_udpos.sh

--- scripts/train_udpos.sh
+++ scripts/train_udpos.sh
@@ -45,7 +45,7 @@ else
 fi
 
 DATA_DIR=$DATA_DIR/$TASK/${TASK}_processed_maxlen${MAX_LENGTH}/
-OUTPUT_DIR="$OUT_DIR/$TASK/${MODEL}-LR${LR}-epoch${NUM_EPOCH}-MaxLen${MAX_LENGTH}/"
+OUTPUT_DIR="$OUT_DIR/$TASK/${MODEL}-LR${LR}-epoch${NUM_EPOCHS}-MaxLen${MAX_LENGTH}/"
 mkdir -p $OUTPUT_DIR
 python3 $REPO/third_party/run_tag.py \
   --data_dir $DATA_DIR \
