a0ffd2453e3d8060f20d5804f45a1b75cf7a54fc
Sebastian Ruder <ruder@google.com>
2020-10-01 05:49:24 -0700

    Removed print messages in evaluate.py
    PiperOrigin-RevId: 334795969

--- evaluate.py
+++ evaluate.py
@@ -35,11 +35,8 @@ def read_tag(file):
       else:
         labels.append(example)
         example = []
-  print(labels)
   if example:
     labels.append(example)
-  print('After appending')
-  print(labels)
   return labels
 
 
@@ -154,7 +151,6 @@ def evaluate_one_task(prediction_file, label_file, task, language=None):
   Both input files contain one example per line as follows:
     ``[label]\t[sentence1]\t[sentence2]``
   """
-  print(task, language)
   predictions = READER_FUNCTION[task](prediction_file)
   labels = READER_FUNCTION[task](label_file)
   if task not in ['bucc2018', 'mlqa', 'tydiqa', 'xquad']:
