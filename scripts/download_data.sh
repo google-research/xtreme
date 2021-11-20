8c2ceb2431a19d95ab756e5e32982e58205a2393
Konstantin Dobler <konstantinj.dobler@gmail.com>
2021-11-20 17:20:14 +0100

    Fix tatoeba branch name: master -> main

--- scripts/download_data.sh
+++ scripts/download_data.sh
@@ -111,14 +111,14 @@ function download_panx {
 
 function download_tatoeba {
     base_dir=$DIR/tatoeba-tmp/
-    wget https://github.com/facebookresearch/LASER/archive/master.zip
-    unzip -qq -o master.zip -d $base_dir/
-    mv $base_dir/LASER-master/data/tatoeba/v1/* $base_dir/
+    wget https://github.com/facebookresearch/LASER/archive/main.zip
+    unzip -qq -o main.zip -d $base_dir/
+    mv $base_dir/LASER-main/data/tatoeba/v1/* $base_dir/
     python $REPO/utils_preprocess.py \
       --data_dir $base_dir \
       --output_dir $DIR/tatoeba \
       --task tatoeba
-    rm -rf $base_dir master.zip
+    rm -rf $base_dir main.zip
     echo "Successfully downloaded data at $DIR/tatoeba" >> $DIR/download.log
 }
 
